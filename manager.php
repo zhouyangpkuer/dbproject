859f5ff592b6962cc8cc376702a767aacbd4969e
Zhouyang <zhou.yang@pku.edu.cn>
2017-05-24 20:24:49 +0800

    algo

--- manager.php
+++ manager.php
@@ -23,8 +23,8 @@
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src='../../assets/js/ie8-responsive-file-warning.js'></script><![endif]-->
     <script src='js/ie-emulation-modes-warning.js'></script>
-
-    <link href='carousel.css' rel='stylesheet'>
+  <link href="css/offcanvas.css" rel="stylesheet">
+    <link href='css/carousel.css' rel='stylesheet'>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src='https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js'></script>
