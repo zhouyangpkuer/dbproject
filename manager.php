3cc233a0e12a9a54a789e0b0ee96d197c222ee3d
Zhouyang <zhou.yang@pku.edu.cn>
2017-05-24 19:42:42 +0800

    algo

--- manager.php
+++ manager.php
@@ -119,11 +119,14 @@
           print $datas_block["BlockName"];
         
           print "</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p>
           <a class='btn btn-default' href='#' role='button'>View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->";
       }
 
   print "</div><!-- /.row -->";
+
 ?>
 
 
