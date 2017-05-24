918713589a420a44b226e81591f25634fba8b060
Zhouyang <zhou.yang@pku.edu.cn>
2017-05-24 17:44:11 +0800

    algo

--- register.php
+++ register.php
@@ -45,6 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		if ($Total == 0)
 		{
 			$insert = "INSERT INTO User(UserId, Password, UserName) values( NULL, '".md5($Password)."', '".$Username."');";
+			print $insert;
 			$sql= mysqli_query($con, $insert);
 			$Succ="Congratulations! You have registered successfully!";
 			// header("Location:./manager.php");
