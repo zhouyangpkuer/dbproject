3e082e7f004d9e8961b928f033c10bf679fb8d2e
Zhouyang <zhou.yang@pku.edu.cn>
2017-05-24 19:52:16 +0800

    algo

--- manager.php
+++ manager.php
@@ -116,8 +116,8 @@
         print "
         <div class='col-lg-4'>
           <img class='img-circle' src='";
-          
-          print $datas_block["BlockName"]."jpg"; 
+
+          print "./pic".$datas_block["BlockName"]."jpg"; 
 
           print "' alt='Generic placeholder image' width='140' height='140'> <h2>";
           print $datas_block["BlockName"];
