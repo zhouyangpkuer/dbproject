0eb89aa4c2a14dd97dfedf606d232d37bd5bde1a
Zhouyang <zhou.yang@pku.edu.cn>
2017-05-24 19:29:52 +0800

    algo

--- manager.php
+++ manager.php
@@ -91,64 +91,30 @@
 	$datas_block = mysqli_fetch_array($result_id_block);
 
 
-    print "
-    <div id='myCarousel' class='carousel slide' data-ride='carousel'>
-      <!-- Indicators -->
-      <ol class='carousel-indicators'>
-        <li data-target='#myCarousel' data-slide-to='0' class='active'></li>
-        <li data-target='#myCarousel' data-slide-to='1'></li>
-        <li data-target='#myCarousel' data-slide-to='2'></li>
-      </ol>
-      <div class='carousel-inner' role='listbox'>
-        <div class='item active'>
-          <img class='first-slide' src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' alt='First slide'>
-          <div class='container'>
-            <div class='carousel-caption'>
-              <h1>";
+  print "  <div class='container marketing'>
 
-			print $datas_block["BlockName"];
+      <!-- Three columns of text below the carousel -->
+      <div class='row'>
+        <div class='col-lg-4'>
+          <img class='img-circle' src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' alt='Generic placeholder image' width='140' height='140'>
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p><a class='btn btn-default' href='#' role='button'>View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class='col-lg-4'>
+          <img class='img-circle' src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' alt='Generic placeholder image' width='140' height='140'>
+          <h2>Heading</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <p><a class='btn btn-default' href='#' role='button'>View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class='col-lg-4'>
+          <img class='img-circle' src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' alt='Generic placeholder image' width='140' height='140'>
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class='btn btn-default' href='#' role='button'>View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->";
 
-              print "
-              </h1>
-              <p>
-              	
-              </p>
-              
-
-              <p><a class='btn btn-lg btn-primary' href='#' role='button'>Sign up today</a></p>
-            </div>
-          </div>
-        </div>
-        <div class='item'>
-          <img class='second-slide' src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' alt='Second slide'>
-          <div class='container'>
-            <div class='carousel-caption'>
-              <h1>Another example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class='btn btn-lg btn-primary' href='#' role='button'>Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div class='item'>
-          <img class='third-slide' src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' alt='Third slide'>
-          <div class='container'>
-            <div class='carousel-caption'>
-              <h1>One more for good measure.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class='btn btn-lg btn-primary' href='#' role='button'>Browse gallery</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a class='left carousel-control' href='#myCarousel' role='button' data-slide='prev'>
-        <span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>
-        <span class='sr-only'>Previous</span>
-      </a>
-      <a class='right carousel-control' href='#myCarousel' role='button' data-slide='next'>
-        <span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>
-        <span class='sr-only'>Next</span>
-      </a>
-    </div><!-- /.carousel --> ";
 
 ?>
 
@@ -198,6 +164,7 @@
     <script src='js/jquery.min.js'></script>
     <script>window.jQuery || document.write('<script src='../../assets/js/vendor/jquery.min.js'><\/script>')</script>
     <script src='js/bootstrap.min.js'></script>
+    <script src="js/holder.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src='js/ie10-viewport-bug-workaround.js'></script>
   </body>
