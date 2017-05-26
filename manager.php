085a4505f4bcf000c23aec60c0ca0db2cd139058
Zhouyang <zhou.yang@pku.edu.cn>
2017-05-26 18:18:09 +0800

    algo

--- manager.php
+++ manager.php
@@ -275,7 +275,7 @@
         
         $topic_url = "./topic.php?topic_id=".$datas_topic["TopicId"]."&user_id=".$Username;
         
-        print "<div class='col-xs-6 col-lg-4'><h2>";
+        print "<div class='col-xs-6 col-lg-4 panel'><h2>";
           print $datas_topic["TopicTitle"];
           print "</h2><p>";
           print $datas_topic["TopicContent"];
